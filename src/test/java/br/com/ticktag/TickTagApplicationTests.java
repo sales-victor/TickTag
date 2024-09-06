d6f3f4b8f4e9480750c77c54c6d05cf7c5f8ef54
Joao Sales <salesvictor@icloud.com>
2024-09-06 09:21:36 -0300

    iniciando projeto

---
+++ src/test/java/br/com/ticktag/TickTagApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ticktag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickTagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
