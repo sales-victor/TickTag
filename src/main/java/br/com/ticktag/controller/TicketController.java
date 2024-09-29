080d02b1f27c91d37ae22661ca36c1cd8a343e4b
ThiagoVini13 <thiago7313vini@gmail.com>
2024-09-29 18:45:35 -0400

    removing unecessary dependencies

--- src/main/java/br/com/ticktag/controller/TicketController.java
+++ src/main/java/br/com/ticktag/controller/TicketController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,10 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.ticktag.dto.EventoFilterDTO;
-import br.com.ticktag.model.EventoVO;
 import br.com.ticktag.model.TicketVO;
-import br.com.ticktag.service.EventoService;
 import br.com.ticktag.service.TicketService;
 import br.com.ticktag.util.ApiResponse;
 
