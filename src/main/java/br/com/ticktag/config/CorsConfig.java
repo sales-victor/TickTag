e4edff247cebd316b95994ec02a7cb9e0adc923d
Joao Sales <salesvictor@icloud.com>
2024-11-02 13:10:21 -0300

    correção erro cors

--- src/main/java/br/com/ticktag/config/CorsConfig.java
+++ src/main/java/br/com/ticktag/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
                         .allowedOrigins("*") // Your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
