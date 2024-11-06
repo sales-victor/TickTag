284135bf23afbc985c630185f661d09b2f02e074
ThiagoVini13 <thiago7313vini@gmail.com>
2024-11-05 21:23:08 -0400

    feat: swagger with authorization

--- src/main/java/br/com/ticktag/config/OpenApiConfig.java
+++ src/main/java/br/com/ticktag/config/OpenApiConfig.java
@@ -1,5 +1,8 @@
 package br.com.ticktag.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +12,13 @@ import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class OpenApiConfig {
-	
+
+	private SecurityScheme createAPIKeyScheme() {
+		return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+				.bearerFormat("JWT")
+				.scheme("bearer");
+	}
+
 	@Bean
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
@@ -21,7 +30,11 @@ public class OpenApiConfig {
 						.license(
 								new License()
 									.name("Apache 2.0")
-									.url("")));
+									.url("")))
+				.addSecurityItem(new SecurityRequirement()
+						.addList("Bearer Authentication"))
+						.components(new Components()
+								.addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
 	}
 
 }
