afa8ec57d1764330ca3d8125b1236f43f10cfc36
ThiagoVini13 <thiago7313vini@gmail.com>
2024-11-08 04:29:53 -0400

    fix: fixing and adding functions to services

--- src/main/java/br/com/ticktag/service/impl/CarrinhoServiceImpl.java
+++ src/main/java/br/com/ticktag/service/impl/CarrinhoServiceImpl.java
@@ -32,6 +32,17 @@ class CarrinhoServiceImpl implements CarrinhoService {
     }
 
     @Override
+    public ApiResponse<CarrinhoVO> findByUser(String email) {
+        try {
+            UsuarioVO usuario = facade.usuarioRepository.findByEmail(email);
+            CarrinhoVO carrinho = usuario.getCarrinho();
+            return ApiResponse.success(carrinho);
+        } catch (Exception e){
+            return ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
     public ApiResponse<CarrinhoVO> saveNewCart(CarrinhoVO carrinhoVO) {
         try {
             String emailUsuario = carrinhoVO.getUsuario().getEmail();
@@ -46,12 +57,57 @@ class CarrinhoServiceImpl implements CarrinhoService {
 
     @Override
     public ApiResponse<CarrinhoVO> updateCart(Long id, CarrinhoVO carrinhoVO) {
-        return null;
+        try {
+            Optional<CarrinhoVO> carrinhoRep = facade.carrinhoRepository.findById(id);
+
+            if(carrinhoRep.isPresent()) {
+                CarrinhoVO carrinho;
+                carrinho = carrinhoVO;
+                carrinho = facade.carrinhoRepository.save(carrinho);
+                return ApiResponse.success(carrinho);
+            } else {
+                return ApiResponse.error(String.format("Carrinho com id: %d não existente", id), HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e){
+            return ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
-    public ApiResponse<String> deleteById(Long idCarrinho) {
-        return null;
+    public ApiResponse<CarrinhoVO> deleteById(Long idCarrinho) {
+        try {
+            Optional<CarrinhoVO> carrinho = facade.carrinhoRepository.findById(idCarrinho);
+
+            if(carrinho.isPresent()) {
+                facade.carrinhoRepository.deleteById(idCarrinho);
+                return ApiResponse.success(carrinho.get());
+            } else {
+                return ApiResponse.error(String.format("Carrinho com id: %d não existente", idCarrinho), HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e){
+            return ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    // TODO: Transform the paid items in Tickets and filter the items according to "Pending" status
+    @Override
+    public ApiResponse<CarrinhoVO> buyCart(Long id, CarrinhoVO carrinho){
+        try {
+            String pago = new String("PAGO");
+            for(ItemCarrinhoVO item : carrinho.getItensCarrinho()){
+                if(item.getStatus().equals(pago)){
+                    continue;
+                } else {
+                    item.setStatus("PAGO");
+                    item = facade.itemCarrinhoRepository.save(item);
+                }
+            }
+            carrinho = facade.carrinhoRepository.save(carrinho);
+            return ApiResponse.success(carrinho);
+        } catch (Exception e) {
+            return ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
--- src/main/java/br/com/ticktag/service/impl/ItemCarrinhoImpl.java
+++ src/main/java/br/com/ticktag/service/impl/ItemCarrinhoImpl.java
@@ -4,20 +4,37 @@ import br.com.ticktag.domain.CarrinhoVO;
 import br.com.ticktag.domain.ItemCarrinhoVO;
 import br.com.ticktag.domain.UsuarioVO;
 import br.com.ticktag.repository.RepositoryFacade;
-import br.com.ticktag.service.CarrinhoService;
 import br.com.ticktag.service.ItemCarrinhoService;
 import br.com.ticktag.util.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
 class ItemCarrinhoImpl implements ItemCarrinhoService {
 
     private  final RepositoryFacade facade;
+
+    @Override
+    public ApiResponse<List<ItemCarrinhoVO>> findAll(){
+        try {
+            List<ItemCarrinhoVO> itensCarrinho = facade.itemCarrinhoRepository.findAll();
+            if(itensCarrinho != null){
+                return ApiResponse.success(itensCarrinho);
+            } else {
+                return ApiResponse.error("Itens não encontrados", HttpStatus.NO_CONTENT);
+            }
+        } catch (Exception e) {
+            return ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
     @Override
     public ApiResponse<ItemCarrinhoVO> findById(Long idItemCarrinho){
@@ -38,23 +55,108 @@ class ItemCarrinhoImpl implements ItemCarrinhoService {
     @Override
     public ApiResponse<ItemCarrinhoVO> saveNewItem(ItemCarrinhoVO itemCarrinho) {
         try {
+            // Verifica se o usuario existe
+            String email = itemCarrinho.getCarrinho().getUsuario().getEmail();
+            UsuarioVO usuario = facade.usuarioRepository.findByEmail(email);
 
+            if(usuario != null){
+                // Verifica se o carrinho esta associado ao usuario
+                CarrinhoVO carrinho = usuario.getCarrinho();
 
+                if(carrinho != null){
+                    // Salva o novo item e o carrinho associado
+                    ItemCarrinhoVO novoItem = saveItem(itemCarrinho, carrinho);
+                    saveCartWithNewItem(carrinho, novoItem);
+                    return ApiResponse.success(novoItem);
+                }
+            }
+
+            System.out.println(itemCarrinho);
+            return ApiResponse.success(itemCarrinho);
         } catch (Exception e) {
-            ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return null;
+    }
+
+    private ItemCarrinhoVO saveItem(ItemCarrinhoVO itemCarrinho, CarrinhoVO carrinho){
+        ItemCarrinhoVO novoItem = ItemCarrinhoVO.builder()
+                                                .carrinho(carrinho)
+                                                .evento(itemCarrinho.getEvento())
+                                                .tipoTicket(itemCarrinho.getTipoTicket())
+                                                .quantidade(itemCarrinho.getQuantidade())
+                                                .status(itemCarrinho.getStatus())
+                                                .build();
+        novoItem = facade.itemCarrinhoRepository.save(novoItem);
+        return novoItem;
+    }
+
+    private CarrinhoVO saveCartWithNewItem(CarrinhoVO carrinho, ItemCarrinhoVO novoItem){
+        if(carrinho.getItensCarrinho() == null){
+            Set<ItemCarrinhoVO> setItens = new HashSet<>();
+            setItens.add(novoItem);
+            carrinho.setItensCarrinho(setItens);
+        } else {
+            carrinho.getItensCarrinho().add(novoItem);
+        }
+        return facade.carrinhoRepository.save(carrinho);
     }
 
 
     @Override
     public ApiResponse<ItemCarrinhoVO> updateItem(Long id, ItemCarrinhoVO itemCarrinho) {
-        return null;
+        try {
+            // Verifica se a quantidade é maior ou igual a 1, para modificá-la
+            if (itemCarrinho.getQuantidade() >= 1) {
+                Optional<ItemCarrinhoVO> item = facade.itemCarrinhoRepository.findById(id);
+
+                // Verifica se o item do carrinho ja existe
+                if (item.isPresent()) {
+                    ItemCarrinhoVO itemAtualizado = atualizaItemCarrinho(item.get(), itemCarrinho);
+                    saveCartWithNewItem(itemAtualizado.getCarrinho(), itemAtualizado);
+                    return ApiResponse.success(itemAtualizado);
+                } else {
+                    return ApiResponse.error("Item não encontrado", HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+
+            } else {
+                return ApiResponse.error("A quantidade mínima é: 1", HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            return ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private ItemCarrinhoVO atualizaItemCarrinho(ItemCarrinhoVO itemAntigo, ItemCarrinhoVO novoItem){
+        ItemCarrinhoVO itemAtualizado = ItemCarrinhoVO.builder()
+                                                    .id(itemAntigo.getId())
+                                                    .carrinho(itemAntigo.getCarrinho())
+                                                    .evento(itemAntigo.getEvento())
+                                                    .tipoTicket(itemAntigo.getTipoTicket())
+                                                    .status(novoItem.getStatus())
+                                                    .quantidade(novoItem.getQuantidade())
+                                                    .build();
+        itemAtualizado = facade.itemCarrinhoRepository.save(itemAtualizado);
+        return itemAtualizado;
     }
 
     @Override
-    public ApiResponse<String> deleteItemById(Long idItemCarrinho) {
-        return null;
+    public ApiResponse<ItemCarrinhoVO> deleteItemById(Long idItemCarrinho) {
+        try {
+            Optional<ItemCarrinhoVO> itemCarrinho = facade.itemCarrinhoRepository.findById(idItemCarrinho);
+            // Verifica se o item do carrinho existe
+            if (itemCarrinho.isPresent()) {
+                deleteItem(itemCarrinho.get());
+                return ApiResponse.success(itemCarrinho.get());
+            } else {
+                return ApiResponse.error("Item não existente", HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e){
+            return ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void deleteItem(ItemCarrinhoVO itemCarrinho){
+        facade.itemCarrinhoRepository.deleteById(itemCarrinho.getId());
     }
 
 }
