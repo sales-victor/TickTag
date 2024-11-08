6ed7f6cfaa02ab5dca2ed7761cbaa8f96f7ce044
Marco Willy <marcowillyazevedo@gmail.com>
2024-11-08 12:31:46 -0300

    feat: bloaqueando acesso padrão.

--- src/main/java/br/com/ticktag/config/SecurityConfig.java
+++ src/main/java/br/com/ticktag/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -17,6 +18,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -35,10 +37,9 @@ public class SecurityConfig {
     private final ServiceFacade facade;
 
     @Bean
-    @Profile("!h2")
+    @Profile("h2")
     public SecurityFilterChain securityFilterChainNoAuth(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**").permitAll()
@@ -50,10 +51,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    @Profile("h2")
+    @Profile("!h2")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return securityFilterChainNoAuth(http);
-        /*
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Adiciona suporte CORS aqui
                 .csrf(AbstractHttpConfigurer::disable)
@@ -67,8 +66,6 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-
-         */
     }
 
     @Bean
