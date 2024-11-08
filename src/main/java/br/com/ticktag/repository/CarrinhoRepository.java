8c4f64811721e817287ad4e93a001d3ba0b00b1c
ThiagoVini13 <thiago7313vini@gmail.com>
2024-11-08 04:33:54 -0400

    feat: adding a filter to carrinho repository

--- src/main/java/br/com/ticktag/repository/CarrinhoRepository.java
+++ src/main/java/br/com/ticktag/repository/CarrinhoRepository.java
@@ -1,9 +1,11 @@
 package br.com.ticktag.repository;
 
 import br.com.ticktag.domain.CarrinhoVO;
+import br.com.ticktag.domain.UsuarioVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarrinhoRepository extends JpaRepository<CarrinhoVO, Long> {
+    CarrinhoVO findByUsuario(UsuarioVO usuario);
 }
